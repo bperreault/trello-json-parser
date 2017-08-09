1e6f3b0787ef83e2821f37e14e715276c264ecd1
bperreault <bcp@faxt.com>
2017-08-09 12:04:43 -0500

    add tab delimited output for giant chart in Excel

--- trello-json-parser.py
+++ trello-json-parser.py
@@ -3,28 +3,78 @@ import os
 import json
 
 # to fill
-json_file = 'trello.json' # the json file to parse
+json_file = 'clubby.json' # the json file to parse
+complete_file = 'prettyjson.json'
+tabdelimited_file = 'allcards_tabbed.txt'
+
 file_extension = '.txt' # the file extension you'd like to use
-end_dir = '/Users/RobinHood/Desktop' # the directory to store your cards
+end_dir = '/Users/Bobbi/GitHub/_Clubby/_ProjectManagement/trello-json-parser-master' # the directory to store your cards 
+
+# convertedjsonfile = "clubbyNoUnicode.json"
+# get rid of unicode chars that break parsing below
+# convertfile = open(json_file, "r") 
+# txt = convertfile.read() 
+# convertfile.close()
+#txt.encode('ascii','ignore')
+#txt = txt.encode('utf-8', 'ignore').replace(u'\u014c\u0106\u014d\u2022','-').decode('ascii')
+# txt = txt.decode('utf-8').replace(u'\u014c\u0106\u014d\u2022','-')
+
+# unic = u''
+# unic += txt
+# txt = unic
+
+# writerfile = open( convertedjsonfile ,"w") 
+# writerfile.write(txt.encode('utf-8')) 
+# writerfile.close() 
+
 
 # open json
 with open(json_file) as data_file:
     data = json.load(data_file)
 
+file_name = os.path.join(end_dir, complete_file)
+target = open(file_name, 'a')
+target.write(json.dumps(data["cards"], sort_keys=True, indent=4, separators=(',', ': '))  + "\r\n")
+target.close()    
+    
 # variables
 cards = data["cards"]
 card_number = 1
 total_cards = len(data["cards"])
 written_cards = 0
+print("Total Cards: " + str(total_cards))
+
+complete_file = 'allcards' + file_extension
+file_name = os.path.join(end_dir, complete_file)
+tabdelimited_filename = os.path.join(end_dir, tabdelimited_file)
+
+target = open(file_name, 'a')
+tabdelimited = open(tabdelimited_filename, 'a')
+    
+tabdelimited.write("Task\tStart\tEnd\t\r\n")
 
 # loop
 for card in cards:
     print("Working on: " + card["name"])
-    complete_file = 'card' + str(card_number) + file_extension
-    file_name = os.path.join(end_dir, complete_file)
-    target = open(file_name, 'a')
-    target.write(card["name"])
-    target.close()
+    
+    taskname = card["name"].encode('utf-8')
+    target.write("\r\n - Task: " + taskname )
+    tabdelimited.write( taskname + '\t')
+    
+    startdatejson = card["pluginData"]
+    if startdatejson is not None:
+        # target.write(str(startdatejson) + "\r\n - Finish Date: ")
+        if len(startdatejson) > 0:
+            startdate1 = str(startdatejson[0]["value"])
+            startdate = json.loads(startdate1)
+            
+            target.write("\r\n - Starts: **** " + str(startdate["fields"]["9DF9YubN-aXHeQd"]) + " **** ")
+            tabdelimited.write( str(startdate["fields"]["9DF9YubN-aXHeQd"]) + '\t')
+            
+        
+    if card["due"] is not None:
+        target.write("\r\n - Finish Date: " + card["due"] + "\r\n")
+        tabdelimited.write( card["due"] + "\t\r\n")
     print("Done!")
     written_cards+=1
     card_number+=1
@@ -32,3 +82,6 @@ for card in cards:
 # message
 print("======")
 print(str(written_cards) + " out of " + str(total_cards) + " written successfully! :)")
+target.close()
+tabdelimited.close()
+   
